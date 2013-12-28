189d6d89b6cc9284af3d43325546e371b39142f0
Admin <admin@Admins-Mac.local>
2013-12-28 13:01:27 +0100

    Initial Commit

---
+++ Zeitmaschine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Zeitmaschine
+//
+//  Created by Admin on 28.12.13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZeitmaschineTests/ZeitmaschineTests.m
@@ -0,0 +1,34 @@
+//
+//  ZeitmaschineTests.m
+//  ZeitmaschineTests
+//
+//  Created by Admin on 28.12.13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZeitmaschineTests : XCTestCase
+
+@end
+
+@implementation ZeitmaschineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
